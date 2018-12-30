5546ebbe1d935811a7f4135847e69a08187c344f
ajbond <olishmollie@gmail.com>
2018-12-30 15:46:54 -0600

    move shirts to data file

--- src/app.js
+++ src/app.js
@@ -1,37 +1,4 @@
 window.onload = function() {
-  let SHIRTS = [
-    new Shirt({
-      id: 0,
-      price: 5,
-      tier: "middle",
-      name: "Unisex Jersey Short Sleeve",
-      imageUrl: "public/assets/3001_06_1.jpg",
-      description:
-        "This updated unisex essential fits like a well-loved favorite, featuring a crew neck, short sleeves and designed with superior combed and ring-spun cotton that acts as the best blank canvas for printing. Offered in a variety of solid and heather cvc colors.",
-      availableSizes: ["XS", "S", "M", "L", "XL"]
-    }),
-    new Shirt({
-      id: 1,
-      price: 7,
-      tier: "top",
-      name: "Tri-Blend Crew",
-      imageUrl: "public/assets/mn1_000032.jpg",
-      description:
-        "Top quality tri-blend crew cut. Superior design for a great feel and a perfect fit.",
-      availableSizes: ["XS", "S", "M", "L", "XL", "2XL"]
-    }),
-    new Shirt({
-      id: 2,
-      price: 4,
-      tier: "bottom",
-      name: "Classic Short Sleeve",
-      imageUrl: "public/assets/G2000-095-SM.png",
-      description:
-        "Classic tee that'll never go out of style. Great shirt at a great price.",
-      availableSizes: ["XS", "S", "M", "L", "XL", "2XL"]
-    })
-  ];
-
   let APP = {
     root: "/",
     quote: new Quote(),
